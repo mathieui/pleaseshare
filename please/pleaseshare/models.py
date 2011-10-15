02a0e317a91183fc2a95360a2a7b0a68664e4417
Mathieu Pasquet <mathieui@mathieui.net>
2011-10-16 00:35:35 +0200

    [enh] Added multifile-torrents through tar uploading

--- please/pleaseshare/models.py
+++ please/pleaseshare/models.py
@@ -2,6 +2,17 @@
 from django.db import models
 
 class Upload(models.Model):
+    """
+    uploader: the name of the uploader (optional)
+    name: name of the file, or the root folder, for a multi-file torrent
+    uuid: unique identifier of the torrent
+    date: date of the upload
+    password: a password to delete the torrent (optional)
+    description: a short description of the torrent
+    size: size of the file/folder
+    multifile: whether the torrent contains several files or not
+    magnet: magnet link of the torrent
+    """
     uploader = models.CharField(max_length=32, default="Anonymous")
     name = models.CharField(max_length=128)
     uuid = models.CharField(max_length=36, primary_key=True)
@@ -9,6 +20,7 @@ class Upload(models.Model):
     password = models.CharField(max_length=32, default="", blank=True)
     description = models.CharField(max_length=400, default="", blank=True)
     size = models.CharField(max_length=8)
+    multifile = models.BooleanField(default=False, blank=True)
     magnet = models.TextField(default="")
 
     def __unicode__(self):
--- please/pleaseshare/views.py
+++ please/pleaseshare/views.py
@@ -5,29 +5,44 @@ from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.sites.models import Site
 from django.conf import settings
-from urllib import quote
 
 # system imports
-from os import mkdir, path, stat
+from os import mkdir, path, stat, walk, remove
 from shutil import rmtree
 from uuid import uuid4
+from urllib import quote
+import tarfile
 
 # local imports
 from models import Upload
 from torrent import maketorrent
 
 
+def get_dir_size(start_path = '.'):
+    """
+    Gets the total size of a directory
+    """
+    total_size = 0
+    for dirpath, dirnames, filenames in walk(start_path):
+        for f in filenames:
+            fp = path.join(dirpath, f)
+            total_size += path.getsize(fp)
+    return total_size
+
 def upload_file(request):
     """
     Get the file upload form
     """
     if request.method == 'POST':
-        obj = handle_uploaded_file(request.FILES[u'please'])
-        obj.uploader = request.POST.get('user', 'Anonymous')
-        obj.description = request.POST.get('description', '')
-        obj.password = request.POST.get('delete', '')
-        obj.save()
-        return HttpResponseRedirect(obj.get_absolute_url())
+        detar = request.POST.get('detar', 'off')
+        detar = (detar == 'on')
+        obj = handle_uploaded_file(request.FILES[u'please'], detar)
+        if obj:
+            obj.uploader = request.POST.get('user', 'Anonymous')
+            obj.description = request.POST.get('description', '')
+            obj.password = request.POST.get('delete', '')
+            obj.save()
+            return HttpResponseRedirect(obj.get_absolute_url())
     return HttpResponseRedirect('/error/')
 
 def delete_file(request):
@@ -48,24 +63,66 @@ def delete_file(request):
     di.update(csrf(request))
     return render_to_response('home.html', di)
 
-def handle_uploaded_file(f):
+def handle_uploaded_file(f, detar=False):
     """
     Write a file to the disk, and in the database
     """
     id = str(uuid4())
     folder = path.join(settings.MEDIA_ROOT, id)
-    file = path.join(folder, f.name)
     mkdir(folder)
+    if detar:
+        _file = extract_tar(f, folder)
+        if _file:
+            size = get_dir_size(folder)
+            multifile = True
+        else:
+            return False
+    else:
+        _file = save_file(f, folder)
+        if _file:
+            size = get_dir_size(folder)
+            multifile = False
+        else:
+            return False
+    size = round(size / (1024.0**2), 2)
+    u = Upload(uuid=id, name=f.name, size=size)
+    t = maketorrent.TorrentMetadata()
+    t.data_path = _file
+    t.comment = "Created with pleaseshare" # self-advertisement
+    t.webseeds = ['http://%s%s' % (Site.objects.get_current().domain, quote(u.get_file()))]
+    u.magnet = t.save(path.join(folder, "%s.torrent" % f.name))
+    u.multifile = multifile
+    u.save()
+    return u
+
+def save_file(f, folder):
+    """
+    Saves a file into a folder
+    """
+    file = path.join(folder, f.name)
     destination = open(file, 'wb+')
     for chunk in f.chunks():
         destination.write(chunk)
     destination.close()
-    size = round(stat(file).st_size / (1024**2), 2)
-    u = Upload(uuid=id, name=f.name, size=size)
-    t = maketorrent.TorrentMetadata()
-    t.data_path = file
-    t.comment = "Created with pleaseshare" # self-advertisement
-    t.webseeds = ['http://%s%s' % (Site.objects.get_current().domain, quote(u.get_file()))]
-    u.magnet = t.save(path.join(folder, "%s.torrent" % f.name))
-    u.save()
-    return u
+    return file
+
+def extract_tar(f, folder):
+    """
+    Extracts a tar file
+    """
+    try:
+        o = tarfile.open(save_file(f, folder))
+    except:
+        return False
+    name = '.'.join(f.name.split('.')[:-1])
+    mkdir(path.join(folder, name))
+    for member in o:
+        # test against file named by stupid people
+        if not member.name.startswith('/') and not \
+                '..' in member.name:
+            o.extract(member, path.join(folder, name))
+    # remove old tar file
+    remove(path.join(folder, f.name))
+    f.name = name
+    return path.join(folder, name)
+

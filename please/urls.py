924eb68b795df76757c0fd5b69658c378cc1fce0
mathieui <mathieui@mathieui.net>
2012-03-04 22:00:53 +0100

    [mod] Make slashes at the end of the url optional

--- please/urls.py
+++ please/urls.py
@@ -12,17 +12,17 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
     url(r'^(?P<msg>[0-9]+)$', 'pleaseshare.views.err_msg', name='err'),
-    url(r'^upload/$', 'pleaseshare.views.upload_file', name='upload'),
-    url(r'^delete/$', 'pleaseshare.views.delete_file', name='delete'),
-    url(r'^view/(?P<object_id>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/$', list_detail.object_detail, {'queryset': Upload.objects.all(), 'template_name': 'single.html'},  name='single'),
+    url(r'^upload/?$', 'pleaseshare.views.upload_file', name='upload'),
+    url(r'^delete/?$', 'pleaseshare.views.delete_file', name='delete'),
+    url(r'^view/(?P<object_id>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/?$', list_detail.object_detail, {'queryset': Upload.objects.all(), 'template_name': 'single.html'},  name='single'),
 
     # static files
-    url(r'^contact$', direct_to_template, {'template': 'contact.html'}, name='Contact'),
-    url(r'^about$', direct_to_template, {'template': 'about.html'}, name='About'),
-    url(r'^tos$', direct_to_template, {'template': 'tos.html'}, name='ToS'),
-    url(r'^help$', direct_to_template, {'template': 'help.html'}, name='Help'),
+    url(r'^contact/?$', direct_to_template, {'template': 'contact.html'}, name='Contact'),
+    url(r'^about/?$', direct_to_template, {'template': 'about.html'}, name='About'),
+    url(r'^tos/?$', direct_to_template, {'template': 'tos.html'}, name='ToS'),
+    url(r'^help/?$', direct_to_template, {'template': 'help.html'}, name='Help'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/?', include(admin.site.urls)),
 )
 
 # In order to follow the good practices, comment this and use a real file server for the static files
